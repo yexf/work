448d4f78688152e8ea7f1230666933e9508a71c1
yexf <yexf_fyf@163.com>
2013-07-17 17:02:38 +0800

    create fdut

---
+++ fdut/test_dui/test.cpp
@@ -0,0 +1,83 @@
+#define WIN32_LEAN_AND_MEAN	
+#define _CRT_SECURE_NO_DEPRECATE
+
+#include <windows.h>
+#include <objbase.h>
+
+#include "..\include\UIlib.h"
+#pragma comment(lib, "..\\bin\\fdut.lib")
+using namespace DuiLib;
+
+class CFrameWindowWnd : public CWindowWnd, public INotifyUI
+{
+public:
+	CFrameWindowWnd() { };
+	LPCTSTR GetWindowClassName() const { return _T("UIMainFrame"); };
+	UINT GetClassStyle() const { return UI_CLASSSTYLE_FRAME | CS_DBLCLKS; };
+	void OnFinalMessage(HWND /*hWnd*/) { delete this; };
+
+	void Notify(TNotifyUI& msg)
+	{
+		if( msg.sType == _T("click") ) {
+			if( msg.pSender->GetName() == _T("closebtn") ) {
+				Close();
+			}
+		}
+	}
+
+	LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
+	{
+		if( uMsg == WM_CREATE ) {
+			m_pm.Init(m_hWnd);
+#if 0
+			CControlUI *pButton = new CButtonUI;
+			pButton->SetName(_T("closebtn"));
+			pButton->SetBkColor(0xFFFFFFFF);
+			m_pm.AttachDialog(pButton);
+#else
+			CDialogBuilder builder;
+			CControlUI* pRoot = builder.Create(_T("test1.xml"), (UINT)0, NULL, &m_pm);
+			ASSERT(pRoot && "Failed to parse XML");
+			m_pm.AttachDialog(pRoot);
+#endif
+			m_pm.AddNotifier(this);
+			return 0;
+		}
+		else if( uMsg == WM_DESTROY ) {
+			::PostQuitMessage(0);
+		}
+#if 0
+		else if( uMsg == WM_NCACTIVATE ) {
+			if( !::IsIconic(m_hWnd) ) {
+				return (wParam == 0) ? TRUE : FALSE;
+			}
+		}
+		else if( uMsg == WM_NCCALCSIZE ) {
+			return 0;
+		}
+		else if( uMsg == WM_NCPAINT ) {
+			return 0;
+		}
+#endif
+		LRESULT lRes = 0;
+		if( m_pm.MessageHandler(uMsg, wParam, lParam, lRes) ) return lRes;
+		return CWindowWnd::HandleMessage(uMsg, wParam, lParam);
+	}
+public:
+	CPaintManagerUI m_pm;
+};
+
+// 程序入口及Duilib初始化部分
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPSTR /*lpCmdLine*/, int nCmdShow)
+{
+	CPaintManagerUI::SetInstance(hInstance);
+	CPaintManagerUI::SetResourcePath(CPaintManagerUI::GetInstancePath());
+
+	CFrameWindowWnd* pFrame = new CFrameWindowWnd();
+	if( pFrame == NULL ) return 0;
+	pFrame->Create(NULL, _T("测试"), UI_WNDSTYLE_FRAME, WS_EX_WINDOWEDGE);
+	pFrame->ShowWindow(true);
+	CPaintManagerUI::MessageLoop();
+
+	return 0;
+}
